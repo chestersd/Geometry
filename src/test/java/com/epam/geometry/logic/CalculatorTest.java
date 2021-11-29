d84d2ae036520749217dc6aba30e3e16c26cbadd
Eugene Kadyko <chester_sd@mail.ru>
2021-11-29 23:23:06 +0300

    added log4j properties

---
+++ src/test/java/com/epam/geometry/logic/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.epam.geometry.logic;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testCalculateAreaShouldCalculateAreaWhenRadiusPositive(){
+        //given
+        Calculator calculator = new Calculator();
+        //when
+
+        //then
+    }
+}
