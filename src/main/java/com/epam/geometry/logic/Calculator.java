70bec57bd2afc263ad3252fb31bf789f85d0b963
Eugene Kadyko <chester_sd@mail.ru>
2021-12-05 14:20:18 +0300

    added Log2J to Calculator

--- src/main/java/com/epam/geometry/logic/Calculator.java
+++ src/main/java/com/epam/geometry/logic/Calculator.java
@@ -3,26 +3,35 @@ package com.epam.geometry.logic;
 import com.epam.geometry.entity.Axis;
 import com.epam.geometry.entity.Ball;
 import com.epam.geometry.entity.Point;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class Calculator {
+
+    private static final Logger LOGGER = LogManager.getLogger(Calculator.class);
 
     private double distance;
 
     public static double calculateArea(Ball ball){
+        LOGGER.info("Start calculating of the area " + ball.toString());
         double area;
+        LOGGER.info("Calculate the area of " + ball + ": " + area);
         area = 4.0 * Math.PI * Math.pow(ball.getRadius(), 2.0);
         return area;
 
     }
 
     public double calculateVolume(Ball ball){
+        LOGGER.info("Start calculating of the volume " + ball.toString());
         double volume;
+        LOGGER.info("Calculate the volume of " + ball + ": " + volume);
         volume = 4.0 / 3.0 * Math.PI * Math.pow(ball.getRadius(), 3.0);
         return volume;
 
     }
 
     public double calculateDistance(Axis axis, Point center){
+        LOGGER.info("Start calculating of the distance " + axis.toString());
         switch (axis) {
             case XY:
                 return center.getValueZ();
@@ -31,23 +40,29 @@ public class Calculator {
             case ZY:
                 return center.getValueX();
         }
+        LOGGER.info("Calculate the distance of " + axis + ": " + distance);
         return distance;
     }
 
     public boolean isCross(Ball ball, Axis axis){
+        LOGGER.info("Start defining of the crossing " + ball.toString());
         Point center = ball.getCenter();
         double distance = calculateDistance(axis, center);
         double radius = ball.getRadius();
+        LOGGER.info("Defined the crossing of " + ball + ": " + axis);
         return Math.abs(distance) <= radius;
     }
 
     public double calculateRatioOfVolume(Ball ball, Point point, Axis axis){
+        LOGGER.info("Start calculating of the ratio " + ball.toString());
         Point center = ball.getCenter();
         double distance = calculateDistance(axis, center);
         double radius = ball.getRadius();
         double height = radius - distance;
         double valueOfFirstSegment = Math.pow(height, 2.0) * Math.PI / 3.0 * (3.0 * radius - height);
         double valueOfSecondSegment = calculateVolume(ball) - valueOfFirstSegment;
-        return valueOfFirstSegment / valueOfSecondSegment;
+        double ratio = valueOfFirstSegment / valueOfSecondSegment;
+        LOGGER.info("Calculate the ratio of " + ball + ": " + ratio);
+        return ratio;
    }
 }
